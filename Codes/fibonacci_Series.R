29f313326de9ba60d5435eafdb78db41c68c094d
Jain-Pratiksha <74486549+Jain-Pratiksha@users.noreply.github.com>
2022-03-13 13:59:13 +0530

    Initial commit

--- Codes/fibonacci_Series.R
+++ Codes/fibonacci_Series.R
@@ -1,4 +1,4 @@
-#fibonacci
+#fibonacci series
 val = as.integer(readline(prompt = "Enter a number: "))
 fseries = numeric(val)
 fseries[1] = 0
@@ -6,7 +6,7 @@ fseries[2] = 1
 #print(fseries)
 
 for (i in 3:val){
-  #now, access pervious to index data and add them
+  #now, access pervious two index data and add them
   fseries[i] = fseries[i-1]+fseries[i-2] 
 }
 print("Fibonacci Series: ")
